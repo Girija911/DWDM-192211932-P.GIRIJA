9c151165cc5e7b819d756416861c827a7734ac3b
Girija911 <122432707+Girija911@users.noreply.github.com>
2024-07-24 12:37:16 +0530

    Update 4BIN(meanmedianbound).R

--- 4BIN(meanmedianbound).R
+++ 4BIN(meanmedianbound).R
@@ -1,25 +1,5 @@
 ht\tps://github.com/Girija911/Compiler-design-192211932
 q()
-z_score_normalized
-# Define the data
-data <- c(200, 300, 400, 600, 1000)
-# Calculate the mean and standard deviation
-mean_val <- mean(data)
-sd_val <- sd(data)
-# Apply z-score normalization
-z_score_normalized <- (data - mean_val) / sd_val
-# Print the result
-z_score_normalized
-# Define the data
-data <- c(200, 300, 400, 600, 1000)
-# Define the min and max values
-min_val <- min(data)
-max_val <- max(data)
-# Apply min-max normalization
-min_max_normalized <- (data - min_val) / (max_val - min_val)
-# Print the result
-min_max_normalized
-cat("Smoothed Data by Bin Boundaries:\n", smoothed_boundaries, "\n")
 # Initial data
 data <- c(11, 13, 13, 15, 15, 16, 19, 20, 20, 20, 21, 21, 22, 23, 24, 30, 40, 45, 45, 45, 71, 72, 73, 75)
 # Sort the data
